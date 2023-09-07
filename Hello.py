c2b720a9902bf7b50365992653143a6af61b228f
Rahimatu Yushawu <76118023+Himaht@users.noreply.github.com>
2023-09-07 07:28:32 -0500

    Add files via upload

--- Hello.py
+++ Hello.py
@@ -1,4 +1,47 @@
-#My first Python Program
-def main():
-   print("Hello World!")
-main()
+
+""" This lab takes place on the planet Pythoid where everything is exactly the same as Earth, except that all months have exactly 30 days.
+    Date: 08/31/2023
+    Title: Calculating Dog and cat age
+    Author: Rahimatu Yushawu#
+"""
+import math
+
+
+age = float(input("How old are you?")) #Take the age of the user
+
+dog_age = (age * 7) #converts age to dog age without the decimal place
+years = (dog_age // 1)  #runs the dog age of humans
+month = (dog_age % 1 * 12 // 1) #converts fractional years to months
+days = (dog_age % 1 * 12 % 1 * 30 // 1) #converts the fractional years to days
+
+#This section calculates in cat years
+
+cat_age = (age / 9) #converts age to cat age without the decimal place
+years_1 = (cat_age // 1)  #runs the cat age of humans
+month_1 = (cat_age % 1 * 12 // 1) #converts fractional years to months
+days_1 = (cat_age % 1 * 12 % 1 * 30 // 1) #converts the fractional years to days
+
+#This section calculates in horse years
+
+horse_age = 3*((age ** 2 - 47) / 7 + 12) #converts age to horse age without the decimal place
+years_2 = (horse_age // 1)  #runs the horse age of humans
+month_2 = (horse_age % 1 * 12 // 1) #converts fractional years to months
+days_2 = (horse_age % 1 * 12 % 1 * 30 // 1) #converts the fractional years to days
+
+print(horse_age)
+print(f' Your dog age in years is {years} {month} months and {days} days')
+
+print(f' Your cat age in years is {years_1} {month_1} months and {days_1} days')
+
+print(f' Your horse age in years is {years_2} {month_2} months and {days_2} days')
+
+
+
+
+
+
+
+
+
+
+
---
+++ Lab3 firstName lastName.py
@@ -0,0 +1,27 @@
+import math
+
+earned_income = int(input("What is you annual income: "))
+marital_status = input("Are you married?   Yes or No :")
+
+
+if earned_income >= 0 and earned_income <= 11000 and marital_status == "No" or "no":
+    singlefiler1_tax = (earned_income * 0.1)
+    print("Your income tax is:$",singlefiler1_tax)
+elif (earned_income >=11001 and earned_income <= 44725) and marital_status == "No" or "no":
+    singlefiler2_tax = ( earned_income * 0.12)
+    print("Your income tax is:$",singlefiler2_tax)
+elif earned_income >= 44726 and earned_income<= 95375 and marital_status == "No" or "no":
+    singlefiler3_tax = (earned_income * 0.22)
+    print("Your income tax is:$", singlefiler3_tax)
+else:
+    if earned_income <= 22000 and marital_status == ("Yes" or "yes"):
+        marriedfiler1_tax = (earned_income * 0.1)
+        print("Your income tax is:$", marriedfiler1_tax)
+    elif earned_income >= 22001 and earned_income <= 89450 and marital_status == "Yes" or "yes":
+        marriedfiler2_tax = (earned_income *0.12)
+        print("Your income tax is:$",marriedfiler2_tax)
+    elif earned_income >= 89451 and earned_income <= 190750 and marital_status == "Yes" or "yes":
+        marriedfiler3_tax = (earned_income * 0.22)
+        print("Your income tax is:$",marriedfiler3_tax)
+    else:
+        print(f'Your anual income was ${earned_income} but not in range')
